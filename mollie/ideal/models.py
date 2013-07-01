d09a0bb497404eb68ac2c3cb293267a39ee4fed9
Martin Bouma <bouma@egoactive.com>
2013-07-01 23:20:47 +0200

    verbeterde mollie afhandeling

--- mollie/ideal/models.py
+++ mollie/ideal/models.py
@@ -44,6 +44,20 @@ class MollieIdealPayment(models.Model):
         abstract = True
         verbose_name = _('Mollie/iDEAL payment')
 
+    def _get_orderxml_from_mollie(self, request_dict):
+        for attempt in range(0,5):
+            # Once in a while mollie returns empty xml. Lets retry a few times
+            parsed_xml = _get_mollie_xml(request_dict)
+            xml_as_string = tostring(parsed_xml.getroot())
+            order_xml = parsed_xml.find('order')
+            erroritem = parsed_xml.find('item')
+            if erroritem and erroritem.attrib.get('type', None) == 'error':
+                raise ValueError("errorcode=%s - %s" % (erroritem.find('errorcode').text, erroritem.find('message').text))
+            if order_xml:
+                return xml_as_string, order_xml
+
+        raise ValueError(_('No order could be found in Mollie-XML'))
+
     def get_order_url(self):
         'Sets up a payment with Mollie.nl and returns an order URL.'
         if settings.MOLLIE_REVERSE_URLS:
@@ -65,35 +79,55 @@ class MollieIdealPayment(models.Model):
             request_dict.update(dict(
                 profile_key=settings.MOLLIE_PROFILE_KEY
             ))
-        parsed_xml = _get_mollie_xml(request_dict)
-        self.fetch_response = tostring(parsed_xml.getroot())
+
+        xml_as_string, orderxml = self._get_orderxml_from_mollie(request_dict)
+        self.fetch_response = xml_as_string
         self.save()
-        order = parsed_xml.find('order')
-        order_url = order.findtext('URL')
-        self.transaction_id = order.findtext('transaction_id')
+        order_url = orderxml.findtext('URL')
+
+        self.transaction_id = orderxml.findtext('transaction_id')
         self.save()
         return order_url
 
     fetch = get_order_url
-        
+
+    '''
+    Mollie respons heeft 2 betaalstatus velden: payed en status:
+
+    payed	[antwoord] 'true' of 'false'. Geeft aan of er betaald is. Let op, wij houden bij of u al eerder succesvol gecheckt heeft. Dus als u de tweede keer checkt sinds de betaling, krijgt u hier 'false'. Zo beschermen we u tegen mogelijke fraude bij uw klanten.
+    status	 [antwoord] Geeft de exacte status zoals deze van de bank terug kwam, mits deze beschikbaar was. Mogelijke statussen zijn:
+        Status	        Omschrijving
+        Success	        De betaling is gelukt
+        Cancelled	    De consument heeft de betaling geannuleerd.
+        Failure	        De betaling is niet gelukt (er is geen verdere informatie beschikbaar)
+        Expired	        De betaling is verlopen doordat de consument niets met de betaling heeft gedaan.
+        CheckedBefore	U heeft de betalingstatus al een keer opgevraagd.
+    '''
     def is_paid(self):
+        # once the status field has a value, be sure not to call mollie again. This will result
+        # in a status 'CheckedBefore'
+        if self.status == 'Success':
+            return True
+        elif self.status:
+            return False
+
         'Checks whether a payment has been made successfully.'
         request_dict = dict(
             a = 'check',
             partnerid = settings.MOLLIE_PARTNER_ID,
             transaction_id = self.transaction_id
         )
-        parsed_xml = _get_mollie_xml(request_dict)
-        self.check_response = tostring(parsed_xml.getroot())
-        self.save()
-        order = parsed_xml.find('order')
-        consumer = order.find('consumer')
+        xml_as_string, order_xml = self._get_orderxml_from_mollie(request_dict)
+        self.check_response = xml_as_string
+        consumer = order_xml.find('consumer')
         if consumer:
             self.consumer_account = consumer.findtext('consumerAccount')
             self.consumer_city = consumer.findtext('consumerCity')
             self.consumer_name = consumer.findtext('consumerName')
-        self.status = order.findtext('status')
-        if order.findtext('payed') == 'true':
+
+        self.status = order_xml.findtext('status')
+        self.save()
+        if self.status == 'Success':
             return True
         return False
 
